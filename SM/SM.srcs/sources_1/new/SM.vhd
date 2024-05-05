61a9d8c1cddbaa3d1ad6c17c7156064fb67aa8c2
rir001 <riveraldrv@gmail.com>
2024-05-04 23:24:47 -0400

    updates

--- SM/SM.srcs/sources_1/new/SM.vhd
+++ SM/SM.srcs/sources_1/new/SM.vhd
@@ -9,44 +9,44 @@ entity SM is
         next_state:     in std_logic;
         back_state:     in std_logic;
 
-        sm:             out std_logic_vector(2 downto 0):= "000";
+        sm_state:             out std_logic_vector(2 downto 0):= "000";
         sub:            out std_logic_vector(3 downto 0):= "0000"
     );
 end SM;
 
 architecture Behavioral of SM is
 
-    signal sm_state:    integer:= 0;
+    signal sm:    integer:= 0;
     signal sub_level:   integer:= 1;
 
 begin
-    process(state, next_state, back_state, sm_state, sub_level)
+    process(state, next_state, back_state, sm, sub_level)
     begin
         if falling_edge(state) then
-            sm_state <= 1;
+            sm <= 1;
             sub_level <= 1;
         end if;
         if state = '0' then
 
             if rising_edge(next_state) then
-                if sm_state >= 5 then
-                    sm_state <= 1;
+                if sm >= 5 then
+                    sm <= 1;
                     sub_level <= 1;
                 else
-                    sm_state <= sm_state + 1;
+                    sm <= sm + 1;
                 end if;
 
             elsif rising_edge(back_state) then
-                sm_state <= sm_state - 1;
-                if sm_state = 4 then
+                sm <= sm - 1;
+                if sm = 4 then
                     sub_level <= sub_level + 1;
                 end if;
             end if;
 
         else
-            sm_state <= 0;
+            sm <= 0;
         end if;
-        sm <= std_logic_vector(to_unsigned(sm_state, sm'length));
+        sm_state <= std_logic_vector(to_unsigned(sm, sm_state'length));
         sub <= std_logic_vector(to_unsigned(sub_level, sub'length));
 
     end process;
