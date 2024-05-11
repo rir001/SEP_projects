94ddcabc23c8935cae1430f0601d13b8b904dfab
Gustavo Blanco <gblanco@uc.cl>
2024-05-11 19:43:07 -0400

    init ip core axi

---
+++ Init_1.0/xgui/Init_v1_0.tcl
@@ -0,0 +1,116 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_param $IPINST -name "C_S00_AXI_DATA_WIDTH" -parent ${Page_0} -widget comboBox
+  ipgui::add_param $IPINST -name "C_S00_AXI_ADDR_WIDTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "C_S00_AXI_BASEADDR" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "C_S00_AXI_HIGHADDR" -parent ${Page_0}
+
+
+}
+
+proc update_PARAM_VALUE.duration1 { PARAM_VALUE.duration1 } {
+	# Procedure called to update duration1 when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.duration1 { PARAM_VALUE.duration1 } {
+	# Procedure called to validate duration1
+	return true
+}
+
+proc update_PARAM_VALUE.duration2 { PARAM_VALUE.duration2 } {
+	# Procedure called to update duration2 when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.duration2 { PARAM_VALUE.duration2 } {
+	# Procedure called to validate duration2
+	return true
+}
+
+proc update_PARAM_VALUE.duration3 { PARAM_VALUE.duration3 } {
+	# Procedure called to update duration3 when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.duration3 { PARAM_VALUE.duration3 } {
+	# Procedure called to validate duration3
+	return true
+}
+
+proc update_PARAM_VALUE.duration4 { PARAM_VALUE.duration4 } {
+	# Procedure called to update duration4 when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.duration4 { PARAM_VALUE.duration4 } {
+	# Procedure called to validate duration4
+	return true
+}
+
+proc update_PARAM_VALUE.C_S00_AXI_DATA_WIDTH { PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to update C_S00_AXI_DATA_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S00_AXI_DATA_WIDTH { PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to validate C_S00_AXI_DATA_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S00_AXI_ADDR_WIDTH { PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to update C_S00_AXI_ADDR_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S00_AXI_ADDR_WIDTH { PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to validate C_S00_AXI_ADDR_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.C_S00_AXI_BASEADDR { PARAM_VALUE.C_S00_AXI_BASEADDR } {
+	# Procedure called to update C_S00_AXI_BASEADDR when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S00_AXI_BASEADDR { PARAM_VALUE.C_S00_AXI_BASEADDR } {
+	# Procedure called to validate C_S00_AXI_BASEADDR
+	return true
+}
+
+proc update_PARAM_VALUE.C_S00_AXI_HIGHADDR { PARAM_VALUE.C_S00_AXI_HIGHADDR } {
+	# Procedure called to update C_S00_AXI_HIGHADDR when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.C_S00_AXI_HIGHADDR { PARAM_VALUE.C_S00_AXI_HIGHADDR } {
+	# Procedure called to validate C_S00_AXI_HIGHADDR
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH { MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH PARAM_VALUE.C_S00_AXI_DATA_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S00_AXI_DATA_WIDTH}] ${MODELPARAM_VALUE.C_S00_AXI_DATA_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH { MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH PARAM_VALUE.C_S00_AXI_ADDR_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.C_S00_AXI_ADDR_WIDTH}] ${MODELPARAM_VALUE.C_S00_AXI_ADDR_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.duration1 { MODELPARAM_VALUE.duration1 PARAM_VALUE.duration1 } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.duration1}] ${MODELPARAM_VALUE.duration1}
+}
+
+proc update_MODELPARAM_VALUE.duration2 { MODELPARAM_VALUE.duration2 PARAM_VALUE.duration2 } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.duration2}] ${MODELPARAM_VALUE.duration2}
+}
+
+proc update_MODELPARAM_VALUE.duration3 { MODELPARAM_VALUE.duration3 PARAM_VALUE.duration3 } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.duration3}] ${MODELPARAM_VALUE.duration3}
+}
+
+proc update_MODELPARAM_VALUE.duration4 { MODELPARAM_VALUE.duration4 PARAM_VALUE.duration4 } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.duration4}] ${MODELPARAM_VALUE.duration4}
+}
+
