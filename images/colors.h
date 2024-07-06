05bbe0799e0b0f823c63f3d3bcd7b6ba038ee525
rir001 <riveraldrv@gmail.com>
2024-07-06 16:29:16 -0400

    o

--- images/colors.h
+++ images/colors.h
@@ -1,4 +1,4 @@
-static const int hp_green = 0x03f0;
+static const int hp_green = 0x83f0;
 static const int hp_yellow = 0x03ff;
 static const int hp_red = 0x000f;
 static const int hp_white = 0xffff;
