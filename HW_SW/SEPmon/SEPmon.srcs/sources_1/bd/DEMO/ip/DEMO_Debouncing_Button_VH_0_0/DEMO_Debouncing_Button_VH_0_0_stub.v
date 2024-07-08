5d8fff0041bf7086bb30e20a606c8c50d36e8c2d
Gustavo Blanco <gblanco@uc.cl>
2024-07-08 05:27:23 -0400

    lux interrupt

---
+++ HW_SW/SEPmon/SEPmon.srcs/sources_1/bd/DEMO/ip/DEMO_Debouncing_Button_VH_0_0/DEMO_Debouncing_Button_VH_0_0_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+// Date        : Sun Jul  7 16:30:13 2024
+// Host        : LAPTOP-5LB4VBU3 running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/gusta/Documents/SEP_projects/HW_SW/SEPmonG3/SEPmon/SEPmon.srcs/sources_1/bd/DEMO/ip/DEMO_Debouncing_Button_VH_0_0/DEMO_Debouncing_Button_VH_0_0_stub.v
+// Design      : DEMO_Debouncing_Button_VH_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "Debouncing_Button_VHDL,Vivado 2020.1" *)
+module DEMO_Debouncing_Button_VH_0_0(button, clk, debounced_button)
+/* synthesis syn_black_box black_box_pad_pin="button,clk,debounced_button" */;
+  input button;
+  input clk;
+  output debounced_button;
+endmodule
